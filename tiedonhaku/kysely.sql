b675b8ed90db6ff648cf6696ad9ea4719dc9c813
Dani <56977415+Hendalf-DEV@users.noreply.github.com>
2025-03-20 13:23:02 +0200

    .

--- tiedonhaku/kysely.sql
+++ tiedonhaku/kysely.sql
@@ -1,7 +1,20 @@
-#Perustehtäviä
-#SELECT * from world.city WHERE Population > 3000000
-#SELECT * from world.country WHERE Name LIKE 'A%'
-#SELECT Name from world.country WHERE LocalName = 'Al-Bahrayn'
-#SELECT Name, Continent, HeadOfState FROM world.country WHERE Continent = 'Africa' AND GovernmentForm = 'Republic'
+# Perustehtäviä
+# SELECT * from city WHERE Population > 3000000;
+# SELECT * from country WHERE Name LIKE 'A%';
+# SELECT Name from country WHERE LocalName = 'Al-Bahrayn;
+# SELECT Name, Continent, HeadOfState FROM country WHERE Continent = 'Africa' AND GovernmentForm = 'Republic';
 
-#Yhteenveto-operaatiot
+# Yhteenveto-operaatiot
+# SELECT MAX(Population) FROM country;
+# SELECT AVG(LifeExpectancy) FROM country;
+# SELECT Continent, COUNT(*) AS 'määrä' FROM country GROUP BY Continent;
+
+# Haku useammasta taulusta
+# SELECT Name, Language, IsOfficial FROM country INNER JOIN countrylanguage ON country.Code = countrylanguage.CountryCode ORDER BY Name ASC;
+# SELECT city.Name, city.Population FROM city INNER JOIN country ON country.Code = city.CountryCode WHERE country.Name = 'Morocco';
+# SELECT Name, Language, GovernmentForm FROM country INNER JOIN countrylanguage ON country.Code = countrylanguage.CountryCode WHERE GovernmentForm = 'Republic' AND Language = 'Spanish';
+
+# Sekalaisia hakuja
+# SELECT Continent AS 'Mantere', COALESCE(AVG(LifeExpectancy), 'Ei tietoa') AS 'Vanha_elinikä', COALESCE(AVG(LifeExpectancy) + 10, 'Ei tietoa') AS 'Uusi_elinikä' FROM country GROUP BY Continent;
+# SELECT DISTINCT Language FROM countrylanguage WHERE Language LIKE 'E%' ORDER BY Language;
+# SELECT city.Name FROM city INNER JOIN country ON city.ID = country.Capital WHERE country.Continent = 'Europe';
