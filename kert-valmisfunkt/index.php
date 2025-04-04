86153bc71c9e5a577d4916c0123d9496550580cc
Dani <56977415+Hendalf-DEV@users.noreply.github.com>
2025-04-04 10:24:33 +0300

    Kertaus ja valmisfunktiot valmis

--- kert-valmisfunkt/index.php
+++ kert-valmisfunkt/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="fi">
 <head>
 <title>Tehtäviä: Kertausta ja valmisfunktioita</title>
 </head>
@@ -14,15 +14,17 @@ echo "<h3>Tehtävä 1</h3>";
 ?>
 
 <form action="index.php" method="post">
-<input type="text" name="luku1" size="5">
-<select name="laskutoimitus">
-<option value="1">+</option>
-<option value="2">-</option>
-<option value="3">*</option>
-<option value="4">/</option>
-</select>
-<input type="text" name="luku2" size="5"><br>
-<input type="submit" name="tehtava1">
+    <label>
+    <input type="text" name="luku1"  size="5">
+    <select name="laskutoimitus">
+    <option value="1">+</option>
+    <option value="2">-</option>
+    <option value="3">*</option>
+    <option value="4">/</option>
+    </select>
+    <input type="text" name="luku2" size="5"><br>
+    <input type="submit" name="tehtava1">
+    </label>
 </form>
 
 <?php
@@ -31,8 +33,34 @@ if(isset($_POST["tehtava1"])) {
 	$luku1 = $_POST["luku1"];
 	$luku2 = $_POST["luku2"];
 	$laskutoimitus = $_POST["laskutoimitus"];	// 1 = yhteenlasku, 2 = vähennyslasku, 3 = kertolasku, 4 = jakolasku
-	
-	// kirjoita koodisi tähän
+    switch($laskutoimitus) {
+        case "1":
+            $tuotto = $luku1 + $luku2;
+            $merkki = "+";
+            break;
+        case "2":
+            $tuotto = $luku1 - $luku2;
+            $merkki = "-";
+            break;
+        case "3":
+            $tuotto = $luku1 * $luku2;
+            $merkki = "*";
+            break;
+        case "4":
+            if($luku2 != 0) {
+                $tuotto = $luku1 / $luku2;
+            } else {
+                $tuotto = "Nollalla ei voi jakaa!";
+            }
+            $merkki = "/";
+            break;
+        default:
+            $tuotto = "Tuntematon laskutoimitus";
+            $merkki = "?";
+            break;
+    }
+
+    echo "<p>$luku1 $merkki $luku2 = $tuotto </p>";
 }
 
 
@@ -40,15 +68,27 @@ if(isset($_POST["tehtava1"])) {
 *  Tehtävä 2  *
 **************/
 echo "<h3>Tehtävä 2</h3>";
-
-// kirjoita koodisi tähän
+$i = 1;
+while ($i <= 10) {
+    echo $i;
+    if ($i < 10) {
+        echo "-";
+    }
+    $i++;
+}
 
 /**************
 *  Tehtävä 3  *
 **************/
 echo "<h3>Tehtävä 3</h3>";
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 2 == 0) {
+        echo "Luku $i on parillinen.<br>";
+    } else {
+        echo "Luku $i on pariton.<br>";
+    }
+}
 
-// kirjoita koodisi tähän
 
 
 /**************
@@ -58,14 +98,15 @@ echo "<h3>Tehtävä 4</h3>";
 
 $lukutaulukko = array(6, 29, 10, 2, 8, 24, 17);
 
-// kirjoita koodisi alle oleviin tulostuslauseisiin (ja tarvittaessa niiden väleihin)
-echo "Taulukon pituus: ";
+echo "Taulukon pituus: " . count($lukutaulukko);
 echo "<br>";
-echo "Taulukon arvot: ";
+echo "Taulukon arvot: " . join(", ", $lukutaulukko);
 echo "<br>";
-echo "Taulukon arvot pienimmästä suurimpaan: ";
+sort($lukutaulukko);
+echo "Taulukon arvot pienimmästä suurimpaan: " . join(", ", $lukutaulukko);
 echo "<br>";
-echo "Taulukon arvot suurimmasta pienimpään: ";
+rsort($lukutaulukko);
+echo "Taulukon arvot suurimmasta pienimpään: " . join(", ", $lukutaulukko);
 echo "<br>";
 
 
@@ -76,8 +117,11 @@ echo "<h3>Tehtävä 5</h3>";
 ?>
 
 <form action="index.php" method="post">
-Anna merkkijono: <input type="text" name="merkkijono">
-<input type="submit" name="tehtava5">
+    <label>
+        Anna merkkijono:
+        <input type="text" name="merkkijono">
+        <input type="submit" name="tehtava5">
+    </label>
 </form>
 
 <?php
@@ -87,11 +131,11 @@ if(isset($_POST["tehtava5"])) {
 	$merkkijono = $_POST["merkkijono"];
 	
 	// kirjoita koodisi alle oleviin tulostuslauseisiin
-	echo "Merkkijono kokonaan pienillä kirjaimilla: <br>";
-	echo "Merkkijono kokonaan isoilla kirjaimilla: <br>";
-	echo "Merkkijono lopusta alkuun: <br>";
-	echo "Merkkijonon pituus: <br>";
-	echo "Jokainen sana omalle rivilleen:<br>";
+	echo "Merkkijono kokonaan pienillä kirjaimilla: " . strtolower($merkkijono) . "<br>";
+	echo "Merkkijono kokonaan isoilla kirjaimilla: " . strtoupper($merkkijono) . "<br>";
+	echo "Merkkijono lopusta alkuun: " . strrev($merkkijono) . "<br>";
+	echo "Merkkijonon pituus: " . strlen($merkkijono) . "<br>";
+	echo "Jokainen sana omalle rivilleen: <br>" . str_replace(" ", "<br>", $merkkijono) . "<br>";
 	
 }
 
@@ -102,8 +146,11 @@ echo "<h3>Tehtävä 6</h3>";
 ?>
 
 <form action="index.php" method="post">
-Anna luvut (välilyönnillä erotettuina): <input type="text" name="lukujono">
-<input type="submit" name="tehtava6">
+    <label>
+        Anna luvut (välilyönnillä erotettuina):
+        <input type="text" name="lukujono">
+        <input type="submit" name="tehtava6">
+    </label>
 </form>
 
 <?php
@@ -113,8 +160,15 @@ if(isset($_POST["tehtava6"])) {
 	$lukujono = $_POST["lukujono"];
 	$lukutaulukko = explode(" ", $lukujono);
 	
-	// kirjoita koodisi tähän
-	
+	$summa = array_sum($lukutaulukko);
+
+    $lukumaara = count($lukutaulukko);
+    $keskiarvo = $summa / $lukumaara;
+
+    echo "Luvut: " . join(", ", $lukutaulukko) . "<br>";
+    echo "Summa: $summa<br>";
+    echo "Keskiarvo: $keskiarvo<br>";
+
 }
 
 ?>
